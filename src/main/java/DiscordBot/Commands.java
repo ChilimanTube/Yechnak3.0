8bb231f7df70457801609a0bd2c3684945d7cbc1
ChilimanTube <chiliman.tube@gmail.com>
2022-06-05 16:18:07 +0200

    Fixed Poll commands and edited few bits.

--- src/main/java/DiscordBot/Commands.java
+++ src/main/java/DiscordBot/Commands.java
@@ -1,51 +1,65 @@
 package DiscordBot;
 
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 public class Commands extends ListenerAdapter {
+    ArrayList<String> studylinks = new ArrayList<>();
+    ArrayList<String> subjects = new ArrayList<>();
     public void onMessageReceived(@NotNull MessageReceivedEvent event){
         String[] args = event.getMessage().getContentRaw().split(" ");
-        ArrayList<String> studylinks = new ArrayList<>();
-        ArrayList<String> subjects = new ArrayList<>();
 
-        Moderation.badWordDetection(args);
-
-
+        for(int i = 0; i < args.length; i++) {
+            if (!Moderation.badWordDetection(args)) {
+                event.getChannel().sendTyping().queue();
+                event.getChannel().sendMessage("Please, watch your language!").queue();
+            }
+        }
+        //PING
         if(args[0].equalsIgnoreCase(Main.prefix + "ping")) {
             event.getChannel().sendMessage("Pong!").queue();
         }
 
+        //PREFIX DISPLAY
         if (args[0].equalsIgnoreCase(Main.prefix + "prefix")){
             event.getChannel().sendTyping().queue();
             event.getChannel().sendMessage("Ayo, my prefix is `!`").queue();
         }
+
+        // COMMAND LIST
         if (args[0].equalsIgnoreCase(Main.prefix + "commands")){
             event.getChannel().sendTyping().queue();
             event.getChannel().sendMessage("Commands:\n" +
                     " `prefix` - displays current prefix \n" +
                     " `commands` - displays all commands \n" +
-                            " `config` - settings").queue();
+                    " `config` - settings \n" +
+                            " ping - command for testing latency \n" +
+                            " poll - command for creating a yes or no poll").queue();
         }
+        // CONFIGURATION SETTINGS
         if (args[0].equalsIgnoreCase(Main.prefix + "config")){
             SlashCommands.data.setName("configuration");
             SlashCommands.data.setDescription("configuration settings");
+
+            //PREFIX
             if (args.length == 2) {
                 if (args[1].equalsIgnoreCase("prefix")){
                     event.getChannel().sendTyping().queue();
                     event.getChannel().sendMessage("Do you want to change the prefix? \n To change a prefix use `config prefix set <prefix>`").queue();
-                    event.getChannel().sendMessage(":warning: **DISCLAIMER**: This command doesn't work yet! :warning: ").queue();
-                    event.getChannel().sendMessage("Probably because Chili is lazy.").queue();
-
+                    event.getChannel().sendMessage(":warning: **This changes the bots prefix globally** :warning:").queue();
                 }
             }
             if (args.length == 4) {
                 if (args[1].equalsIgnoreCase("prefix")){
                     if (args[3].equalsIgnoreCase("set")){
-                        // Develop a setter
+                        event.getChannel().sendTyping().queue();
+                        Main.prefix = args[4];
+                        event.getChannel().sendMessage("New global prefix was set.").queue();
                     }
                 }
             }else if (args.length == 1){
@@ -61,7 +75,7 @@ public class Commands extends ListenerAdapter {
 
         if(args[0].equalsIgnoreCase(Main.prefix + "subjects"))  {
            if (args.length == 1) {
-               if (subjects.size() > 0) {
+               if (subjects.size() >= 1) {
                    event.getChannel().sendTyping().queue();
                    event.getChannel().sendMessage("**Available commands:** \n" +
                            " list - displays all subjects").queue();
@@ -82,12 +96,97 @@ public class Commands extends ListenerAdapter {
                    }
                }
                if (args[1].equalsIgnoreCase("list")) {
-                   for (int i = 0; i < subjects.size(); i++) {
-                       event.getChannel().sendTyping().queue();
-                       event.getChannel().sendMessage(subjects.get(i)).queue();
+                   event.getChannel().sendTyping().queue();
+                   if(subjects.size() == 0){
+                       event.getChannel().sendMessage("No subjects were set. Type-in `" + Main.prefix + "subject add` to add one!").queue();
+                   }
+                   else {
+                       for (int i = 0; i < subjects.size(); i++) {
+                           event.getChannel().sendMessage(subjects.get(i)).queue();
+                       }
                    }
                }
            }
+        }
+
+        // second try poll
+        if (args[0].equalsIgnoreCase(Main.prefix + "poll")) {
+            String ping = "";
+            String pollMessage = "";
+            // Ping @everyone
+            if(args[1].equalsIgnoreCase("pe")){
+                ping = "@everyone ";
+                //YesOrNo Poll
+                if(args[2].equalsIgnoreCase("yn")){
+                    StringBuffer buffer = new StringBuffer();
+                    for(int i = 3; i < args.length; i++) {
+                        buffer.append(args[i] + " ");
+                    }
+                    event.getChannel().sendTyping().queue();
+                    pollMessage = buffer.toString();
+                    event.getChannel().sendMessage(ping + pollMessage).queue(message -> {
+                        message.addReaction("✅").queue();
+                        message.addReaction("❌").queue();
+                    });
+                }
+            }
+            // Ping @here
+            if(args[1].equalsIgnoreCase("ph")){
+                ping = "@here ";
+                //YesOrNo Poll
+                if(args[2].equalsIgnoreCase("yn")){
+                    StringBuffer buffer = new StringBuffer();
+                    for(int i = 3; i < args.length; i++) {
+                        buffer.append(args[i] + " ");
+                    }
+                    event.getChannel().sendTyping().queue();
+                    pollMessage = buffer.toString();
+                    event.getChannel().sendMessage(ping + pollMessage).queue(message -> {
+                        message.addReaction("✅").queue();
+                        message.addReaction("❌").queue();
+                    });
+                }
+            }if(args[1].equalsIgnoreCase("yn")){
+                ping = "";
+                //YesOrNo Poll
+                    StringBuffer buffer = new StringBuffer();
+                    for(int i = 2; i < args.length; i++) {
+                        buffer.append(args[i] + " ");
+                    }
+                    event.getChannel().sendTyping().queue();
+                    pollMessage = buffer.toString();
+                    event.getChannel().sendMessage(ping + pollMessage).queue(message -> {
+                        message.addReaction("✅").queue();
+                        message.addReaction("❌").queue();
+                    });
+            }
+        }
+
+        //TIMEOUT
+        if(args[0].equalsIgnoreCase(Main.prefix + "timeout")){
+            if(args.length == 2){
+                event.getChannel().sendTyping().queue();
+                String id = args[1];
+                id.replace("<@", "").replace(">", "");
+                args[1].replace("<@", "").replace(">", "");
+                Member member = event.getGuild().getMemberById(id);
+                member.timeoutFor(1, TimeUnit.DAYS);
+                event.getChannel().sendMessage("Timeout for user @" + member + " has been set. Duration unset.").queue();
+            } else {
+                event.getChannel().sendMessage("Incorrect command. Use `" + Main.prefix + "timeout @<member>`").queue();
+            }
+        }
+        //REMOVE TIMEOUT
+        if(args[0].equalsIgnoreCase(Main.prefix + "remove") && args[1].equalsIgnoreCase("timeout")){
+            if(args.length == 3){
+                Member member = event.getGuild().getMemberById(args[1].replace("<@", "").replace(">",""));
+                if(member.isTimedOut()){
+                    member.removeTimeout();
+                    event.getChannel().sendMessage("Timeout for user @" + member + " was removed.").queue();
+                }else{
+                    event.getChannel().sendMessage("User @" + member + "  is not timeouted.").queue();
+                }
+            }
         }
     }
 }
--- src/main/java/DiscordBot/Moderation.java
+++ src/main/java/DiscordBot/Moderation.java
@@ -1,2 +1,56 @@
-package DiscordBot;public class Moderation {
+package DiscordBot;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Moderation {
+    static Scanner scanner;
+    static ArrayList<String> wordList = new ArrayList<>();
+    /*{
+        try {
+            File badWords = new File("src/badwords.txt");
+            scanner = new Scanner(badWords);
+            while (scanner.hasNextLine()) {
+                String word = scanner.nextLine();
+                wordList.add(word);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred while loading the file.");
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }*/
+
+    public static boolean badWordDetection(String @NotNull [] words){
+        {
+            try {
+                File badWords = new File("src/badwords.txt");
+                scanner = new Scanner(badWords);
+                while (scanner.hasNextLine()) {
+                    String word = scanner.nextLine();
+                    wordList.add(word);
+                }
+            } catch (FileNotFoundException e) {
+                System.out.println("An error occurred while loading the file.");
+                e.printStackTrace();
+                throw new RuntimeException(e);
+            }
+        }
+
+        for(int i = 0; i < words.length; i++){
+            for(int j = 0; j < wordList.size(); j++){
+                if (words[i].equalsIgnoreCase(wordList.get(j))){
+                    return false;
+                }else{
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
 }
